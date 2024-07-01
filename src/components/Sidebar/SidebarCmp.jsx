be6ee7c946cc18c3c0925bf195b99468b5afda4d
MoRashid <MoRashid>
2024-07-02 00:42:12 +0100

    test: deploy workflow

--- src/components/Sidebar/SidebarCmp.jsx
+++ src/components/Sidebar/SidebarCmp.jsx
@@ -10,7 +10,7 @@ export default function Sidebar({
   return (
     <aside className="w-1/3 rounded-r-xl bg-stone-900 px-8 py-16 text-stone-50 md:w-72">
       <h2 className="mb-8 font-bold uppercase text-stone-200 md:text-xl">
-        Your Projects
+        Your Projects ----
       </h2>
       <Button onClick={onCreateProject} >
         + Add Project
